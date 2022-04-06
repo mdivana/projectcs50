bd36bce1ea3ef6206d329d6839aa8761fa98e88a
Nika Mdivani <nikamdivani@icloud.com>
2022-04-06 19:34:48 +0400

    added register

--- blog/views.py
+++ blog/views.py
@@ -1,13 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Post
-
+from email import message
 
 def home(request):
     context = {
         'posts': Post.objects.all()
     }
-    return render(request, 'blog/home.html')
+    return render(request, 'blog/home.html', context)
 
 def about(request):
     return render(request, 'blog/about.html')
---
+++ users/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render, redirect
+from email import message
+from django.contrib.auth.forms import UserCreationForm
+from .forms import UserRegisterForm
+
+def register(request):
+    if request.method == "POST":
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            message.success(request, f'Account created! You are now able to log in')
+            return redirect('blog-home')
+    else:
+        form = UserRegisterForm()
+    return render(request, "users/register.html", {
+        "form": form,
+    })
