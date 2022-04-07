51bd5a0eaccb0df39cbe061f1153b08777d4eba4
Nika Mdivani <nikamdivani@icloud.com>
2022-04-07 20:23:08 +0400

    add profile update functinality

--- users/views.py
+++ users/views.py
@@ -1,17 +1,30 @@
 from django.shortcuts import render, redirect
-from email import message
+from django.contrib import messages
 from django.contrib.auth.forms import UserCreationForm
-from .forms import UserRegisterForm
+from django.contrib.auth.decorators import login_required
+from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
 
 def register(request):
     if request.method == "POST":
         form = UserRegisterForm(request.POST)
         if form.is_valid():
             form.save()
-            message.success(request, f'Account created! You are now able to log in')
+            messages.success(request, f'Account created! You are now able to log in')
             return redirect('login')
     else:
         form = UserRegisterForm()
     return render(request, "users/register.html", {
         "form": form,
     })
+
+@login_required
+def profile(request):
+    user_form = UserUpdateForm()
+    profile_form = ProfileUpdateForm()
+
+    context = {
+        'user_form': user_form,
+        'profile_form': profile_form
+    }
+
+    return render(request, 'users/profile.html', context)
