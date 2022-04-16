11097e69bd257874498a8c2b31f8346f70bce4bc
Nika Mdivani <nikamdivani@icloud.com>
2022-04-16 18:00:22 +0400

    final steps

--- users/views.py
+++ users/views.py
@@ -20,16 +20,16 @@ def register(request):
 @login_required
 def profile(request):
     if request.method == "POST":
-        user_form = UserUpdateForm(request.POST)
-        profile_form = ProfileUpdateForm(request.POST, request.FILES)
+        user_form = UserUpdateForm(request.POST, instance=request.user)
+        profile_form = ProfileUpdateForm(request.POST, request.FILES, instance=request.user.profile)
         if user_form.is_valid() and profile_form.is_valid():
             user_form.save()
             profile_form.save()
             messages.success(request, f'Your account has been updated!')
             return redirect('profile')
     else:
-        user_form = UserUpdateForm()
-        profile_form = ProfileUpdateForm()
+        user_form = UserUpdateForm(instance=request.user)
+        profile_form = ProfileUpdateForm(instance=request.user.profile)
 
     context = {
         'user_form': user_form,
